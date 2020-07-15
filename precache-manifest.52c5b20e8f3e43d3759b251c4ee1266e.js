b055e3f01062f77dd8f866d25e6fc9cf15c85365
ov-avinash <avinash.dewangan@techment.com>
2020-07-16 01:19:59 +0530

    Updates

---
+++ precache-manifest.52c5b20e8f3e43d3759b251c4ee1266e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca07a23639e9f44f12f4d790d5e33bd",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "07f8dcadcafbdef391ae",
+    "url": "/react-deploy/static/css/main.b312a01f.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/react-deploy/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07f8dcadcafbdef391ae",
+    "url": "/react-deploy/static/js/main.bc8679e0.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  },
+  {
+    "revision": "3847f4c20da66be49a9857516f508527",
+    "url": "/react-deploy/static/media/internet.3847f4c2.png"
+  }
+]);
